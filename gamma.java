8bee8dae1c25ec7fe070a6d0e9f0f2b2af01e76f
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-07-22 18:43:38 -0400

    Add files via upload

---
+++ gamma.java
@@ -0,0 +1,11 @@
+import java.lang.Object.*;
+public class gamma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 1.456;
+		double y = x* Math.log10(x);
+		System.out.println(y);
+	}
+
+}
