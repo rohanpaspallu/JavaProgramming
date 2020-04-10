57ea81b7c5d1e390c352dfc2bdafbf4d9f4085a3
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2020-04-09 21:38:22 -0400

    add two numbers from array and compare to target
    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.

--- Solution.java
+++ Solution.java
@@ -1,60 +1,84 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer>al = new ArrayList<Integer>();
+        for(int i=0;i<nums.length-1;i++){  
+//            if(nums[i]<=target){
+                    for(int j=1;j<nums.length;j++){
+                
+//                    	if(nums[j]<=target){
+//                    		System.out.println(nums[i]+ " "+ nums[j]);
+                    	
+                    	
+	                        if(nums[i]+nums[j]==target && i!=j){
+//	                        	if(al.contains(i)&&al.contains(j)) {
+//	                        		continue;
+//	                        	}
+//	                        	else if(al.contains(i)&& !al.contains(j)) {
+//	                        		al.add(j);
+//	                        	}
+//	                        	else if(!al.contains(i)&& al.contains(j)) {
+//	                        		al.add(i);
+//	                        	}
+//	                        	else {
+//	                        		al.add(i);
+//	                        		al.add(j);
+//	                        	}
+	                            
+	                        	
+	                        	if(al.contains(i)) {
+	                        		continue;
+	                        	}
+	                        	else {
+	                        		al.add(i);
+	                        	}
+	                        	
+	                        	if(al.contains(j)) {
+	                        		continue;
+	                        	}
+	                        	else {
+	                        		al.add(j);
+	                        	}
+	                            
+	                        }
+	                        else {
+	                        	continue;
+	                        }
+//                    	}
+//                    	else {
+//                    		continue;
+//                    	}
+                    }   
+//            }
+//            else {
+//            	continue;
+//            }
+        }
+        int[] x = new int[al.size()];
+        for(int i=0;i<x.length;i++) {
+        	x[i]= al.get(i);
+        }   
+        return x;
+    }
+	    public static void main(String []args){
+	        int [] nums = {-1, -2, -3, -4, -5};
+	        int target = -8;
+	        
+	        Solution s = new Solution();
+	        int []sum = s.twoSum(nums, target);
+	        
+	        
+	        List<Integer> l = new ArrayList<>(sum.length);
 
-	public Solution() {
+			for (int i : sum) {
+				l.add(Integer.valueOf(i));
+			}
+	        
+			System.out.println(l);
+	    }
 	}
-	
-
-     public static void main(String []args){
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine().toLowerCase();
-        char arr[] = str.toCharArray();
-        ArrayList<String> al = new ArrayList<String>();
-        for(int i=1;i<arr.length;i++) {
-        	al.add(str.substring(0,i));
-        	al.add(str.substring(i,str.length()));
-        }
-        int count=0;
-        //System.out.println(al);
-        
-        
-        String arr1[] =al.toArray(new String[al.size()]);
-        for(int i=0;i<arr.length;i++) {
-        	//System.out.println(arr[i]);
-        }
-        
-        
-        
-        for(int i=0;i<arr1.length-1;i=i+2) {
-        	String str1= arr1[i];
-        	String str2= arr1[i+1];
-        	int count1=0;
-        	int count2=0;
-
-
-            
-//            System.out.println(count1+" "+count2);
-            
-            
-        	count1 = (int) arr1[i].chars().distinct().count();
-        	count2 = (int) arr1[i+1].chars().distinct().count();
-//        	System.out.println(count1 +" "+ count2);
-        	if(count1==count2) {
-        		count++;
-        	}
-        	else {
-        		continue;
-        	}
-        	
-        	
-        }
-        
-        
-        
-        System.out.println(count);
-
-     }
-}
